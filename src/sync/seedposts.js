ff9fc9f8e23ca78d740b1c9e9418c151c323f2a5
Dani Akash <DaniAkash@users.noreply.github.com>
2020-04-12 11:49:20 +0530

    created seed data & clear method

--- src/sync/seedposts.js
+++ src/sync/seedposts.js
@@ -1,67 +1,98 @@
+require("../config/db");
 const faker = require("faker");
+const Author = require("../models/author");
+const Post = require("../models/post");
 
 const seedPosts = () => {
   const blogData = [
-    [
-      {
-        author: "Dani",
-        posts: [
-          {
-            title: faker.lorem.word(),
-            content: faker.lorem.paragraphs()
-          },
-          {
-            title: faker.lorem.word(),
-            content: faker.lorem.paragraphs()
-          },
-          {
-            title: faker.lorem.word(),
-            content: faker.lorem.paragraphs()
-          }
-        ]
-      }
-    ],
-    [
-      {
-        author: "John",
-        posts: [
-          {
-            title: faker.lorem.word(),
-            content: faker.lorem.paragraphs()
-          },
-          {
-            title: faker.lorem.word(),
-            content: faker.lorem.paragraphs()
-          },
-          {
-            title: faker.lorem.word(),
-            content: faker.lorem.paragraphs()
-          }
-        ]
-      }
-    ],
-    [
-      {
-        author: "Sam",
-        posts: [
-          {
-            title: faker.lorem.word(),
-            content: faker.lorem.paragraphs()
-          },
-          {
-            title: faker.lorem.word(),
-            content: faker.lorem.paragraphs()
-          },
-          {
-            title: faker.lorem.word(),
-            content: faker.lorem.paragraphs()
-          }
-        ]
-      }
-    ]
+    {
+      author: "Dani",
+      posts: [
+        {
+          title: faker.lorem.word(),
+          content: faker.lorem.paragraphs()
+        },
+        {
+          title: faker.lorem.word(),
+          content: faker.lorem.paragraphs()
+        },
+        {
+          title: faker.lorem.word(),
+          content: faker.lorem.paragraphs()
+        }
+      ]
+    },
+    {
+      author: "John",
+      posts: [
+        {
+          title: faker.lorem.word(),
+          content: faker.lorem.paragraphs()
+        },
+        {
+          title: faker.lorem.word(),
+          content: faker.lorem.paragraphs()
+        },
+        {
+          title: faker.lorem.word(),
+          content: faker.lorem.paragraphs()
+        }
+      ]
+    },
+    {
+      author: "Sam",
+      posts: [
+        {
+          title: faker.lorem.word(),
+          content: faker.lorem.paragraphs()
+        },
+        {
+          title: faker.lorem.word(),
+          content: faker.lorem.paragraphs()
+        },
+        {
+          title: faker.lorem.word(),
+          content: faker.lorem.paragraphs()
+        }
+      ]
+    }
   ];
 
-  console.log(blogData);
+  blogData.forEach(item => {
+    const author = new Author({
+      name: item.author
+    });
+
+    author
+      .save()
+      .then(response => {
+        const authorId = response._id;
+        if (authorId) {
+          item.posts.forEach(post => {
+            const newPost = new Post({
+              title: post.title,
+              content: post.content,
+              author: authorId
+            });
+            newPost
+              .save()
+              .then(console.log)
+              .catch(console.error);
+          });
+        }
+      })
+      .catch(console.error);
+  });
+};
+
+const clearDB = () => {
+  Author.remove({})
+    .then(console.log)
+    .catch(console.error);
+
+  Post.remove({})
+    .then(console.log)
+    .catch(console.error);
 };
 
 seedPosts();
