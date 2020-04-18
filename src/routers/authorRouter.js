10c079af58617af3555f80b14350915160cf9daf
Dani Akash <DaniAkash@users.noreply.github.com>
2020-04-18 10:21:04 +0530

    created author get request

--- src/routers/authorRouter.js
+++ src/routers/authorRouter.js
@@ -1,5 +1,16 @@
 const express = require("express");
+const Author = require("../models/author");
 
 const authorRouter = express.Router();
+
+authorRouter.get("/", async (request, response) => {
+  try {
+    const authors = await Author.find({});
+    response.status(200).json({ authors });
+  } catch (e) {
+    console.error(e);
+    response.status(500).send("Internal Server Error");
+  }
+});
 
 module.exports = authorRouter;
